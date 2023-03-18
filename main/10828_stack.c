ac279c741523853c055fb266eaaa67c75ad00684
yjh2315 <119277442+yjh2315@users.noreply.github.com>
2023-03-18 12:32:49 +0000

    10828_stack

---
+++ main/10828_stack.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct STACK{
+    int top;
+    int* array;
+}S;
+
+void push(struct STACK *stack, int val);
+void pop(struct STACK *stack);
+void size(struct STACK *stack);
+void empty(struct STACK *stack);
+void top(struct STACK *stack);
+
+
+int main(void){
+    int inputNum;
+    scanf("%d",&inputNum);
+    struct STACK stack = {0,(int*)(malloc(sizeof(int)*(inputNum+1)))};
+    for(int i=0; i<inputNum; i++){
+        char command[10];
+        scanf("%s",&command);
+        if(strcmp(command,"push")==0){
+            int val;
+            scanf("%d",&val);
+            push(&stack,val);
+        }else if((strcmp(command,"pop"))==0){
+            pop(&stack);
+        }else if((strcmp(command,"size"))==0){
+            size(&stack);
+        }else if((strcmp(command,"empty"))==0){
+            empty(&stack);
+        }else{
+            top(&stack);
+        }
+    }
+}
+
+void push(struct STACK *stack, int val){
+    (*stack).array[(*stack).top]=val;
+    (*stack).top++;
+}
+
+void pop(struct STACK *stack){
+    if((*stack).top==0){
+        printf("-1\n");
+    }else{
+        (*stack).top--;
+        int a = (*stack).array[(*stack).top]; 
+        (*stack).array[(*stack).top] = 0;
+        printf("%d\n",a);
+    }
+}
+
+void size(struct STACK *stack){
+    if((*stack).top>=0){
+        printf("%d\n",(*stack).top);
+    }
+    else{
+        printf("0\n");
+    }
+}
+
+void empty(struct STACK *stack){
+    if((*stack).top<=0){
+        printf("%d\n",1);
+    }else{
+        printf("%d\n",0);
+    }
+}
+
+void top(struct STACK *stack){
+    if((*stack).top==0){
+        printf("-1\n");
+    }else{
+        int temp = (*stack).top;
+        int a = (*stack).array[--temp]; 
+        printf("%d\n",a);
+    }
+}
