878026cb09a1a80d62df6865e38f2821f7a047a2
Jihun Yun <yunjyhoon@cau.ac.kr>
2023-03-28 23:35:50 +0900

    success

--- main/2635.c
+++ main/2635.c
@@ -5,18 +5,20 @@ int main(){
     scanf("%d",&in);
     
     int d1 = in;
-    int d2 = in/2;
+    int d2 = 1;
     int d3 = 0;
 
+    int added = 0;
     int cnt = 2;
     int tCnt = 2;
 
-    int arr[100]={0,};
-    int tmp[100]={0,};
-    while(d2++<in){
-        arr[0] = d1;
-        arr[1] = d2;
+    int arr[30000]={0,};
+    int tmp[30000]={0,};
+    while(d2<=in){
+        tmp[0] = d1;
+        tmp[1] = d2;
         int i = 2;
+        
         
         while(1){
             d3 = d1-d2;
@@ -29,19 +31,20 @@ int main(){
         }
 
         if(cnt<tCnt){
-            for(int i=0; i<tCnt; i++){
-                arr[i] = tmp[i];
+            for(int j=0; j<tCnt; j++){
+                arr[j] = tmp[j];
             }
             cnt = tCnt;
         }
+
+        added++;
+        d1 = in;
+        d2 = 0 + added;
+        tCnt = 2;
     }
 
+    printf("%d\n",cnt);
     for(int i=0; i<cnt; i++){
         printf("%d ",arr[i]);
     }
-    
-
-    
-
-    
 }
